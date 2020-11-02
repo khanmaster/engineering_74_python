7f2e829e6ac0cc05c7e16e01adb21fe3ee7f4cc9
khanmaster <eashkhan@hotmail.com>
2020-11-02 15:05:01 +0000

    python variables with input and type()

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 ## pycharm setup
 ### python 3.7
 ### Python Variable
+
+
+- What is a variable
+- Dynamically typed languages
+- Overwriting variables
+- Getting user input from the command line
+
+# Variable:
+- is a place holder to store the data
+
+# Sudo coding
+- basically it is a translation of your code
--- python_variables.py
+++ python_variables.py
@@ -1,1 +1,50 @@
-print("test")
+# # How can we create a variable
+# # print("Shahrukh")
+
+
+name = "Shahrukh" # declaring string variable
+
+# creating a variable called name to store user name
+age = 18 # Integer
+
+hourly_wage = 5 # Integer
+
+travel_allowance = 2.4 # float
+
+# Type of Variable in python
+# String values, Int, Float and Boolean
+
+print(type(age))
+
+
+
+print(type(travel_allowance))
+print(hourly_wage)
+
+print(type(name))
+# How can we find out the type of variable
+# we have type() method in Python that give us the actual data type
+# How can we take user data
+# We can use a method called input() to get data from user
+
+
+# name = input("Please enter your name ")
+# # getting user data by using input()method
+#
+# print(name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
