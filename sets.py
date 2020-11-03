e8db3829a602b914de66c13e62b47a7750dc9109
khanmaster <eashkhan@hotmail.com>
2020-11-03 15:00:39 +0000

    sets lesson added

---
+++ sets.py
@@ -0,0 +1,17 @@
+# set - and frozen sets data collection with sets
+# indexing - THERE IS NO INDEXING IN SETS
+# sets are unordered
+# Syntax: {} brackets for sets
+
+car_parts = {"Wheels", "Doors", "Radio"}
+print(car_parts)
+
+# managing data with sets
+
+# add an item to the set
+car_parts.add("seat belt")
+print(car_parts)
+
+# delete and item from the set
+car_parts.remove("Wheels")
+print(car_parts)
